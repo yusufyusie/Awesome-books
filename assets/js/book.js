584a60966b9f5e2fc134df247ece7b332701bb8f
yusufyusie <yesuffenta023@gmail.com>
2023-07-03 23:46:41 +0300

    Add th user interface for my book store form

--- assets/js/book.js
+++ assets/js/book.js
@@ -3,10 +3,11 @@ const form = document.querySelector('.add-book-form');
 const titleInput = document.querySelector('.title-book')
 const authorInput = document.querySelector('.author-book')
 const addButton = document.querySelector('.add-btn');
+const errorMsg = document.querySelector('.error-message');
 // Declare Main Class
-class Book{
-    static arraylist = [];
-    constructor(title, author,id){
+class Book {
+     static arraylist = [];
+    constructor(title, author, id){
         this.title = title;
         this.author = author;
         this.id = id;
@@ -17,9 +18,9 @@ class Book{
 // LOCAL STORAGE
 class LocalStore {
     static storeBooks(){
-        let books = Book.arraylist = [...Book.arraylist, this];
+        let books;
         const storeData = localStorage.getItem('local');
-        if(storeData === null) {
+        if(!storeData) {
             books = localStorage.setItem('Added Books', JSON.stringify([]));
         } else {
             books = JSON.parse(storeData);
@@ -35,6 +36,31 @@ class LocalStore {
         }
     }
 }
+
+class UI {
+    static showBooks() {
+      const books = LocalStore.getBooks();
+      books.forEach((newBook) => {
+        UI.addBookToList(newBook);
+      });
+    }
+  
+    static addBookToList(newBook) {
+      newBook.id = newId;
+      const bookInfo = `
+        <div id="${newId}">
+          <p class="book-position">"<span class="">${newBook.title}</span>" by <span class="">${newBook.author}</span></p>
+          <button id="${newId}" class="btn-remove">Remove</button>
+        </div>
+      `;
+      bookDetail.innerHTML += bookInfo;
+      newId += 1;
+    }
+  
+    static deleteBookFromList(e) {
+      e.parentElement.remove();
+    }
+  }
 
 // Add books
 addButton.addEventListener('click', (e) => {
@@ -52,5 +78,12 @@ addButton.addEventListener('click', (e) => {
     addNewBook = new Book(newTitle, newAuthor, newId);
     if(newTitle.length !==0 && newAuthor.length !==0){
         localStorage.addBooks(addNewBook);
-    }
-})
+        UI.addBookToList(newBook);
+        errorMsg.style.display = 'none';
+    }  else {
+        errorMsg.style.display = 'block';
+      }
+      form.reset();
+    });
+
+    document.addEventListener('DOMContentLoaded', UI.showBooks);
