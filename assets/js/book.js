0a1a320e12bad10ea9c80d6ddef4a8eed4ddeab2
yusufyusie <yesuffenta023@gmail.com>
2023-07-03 20:05:05 +0300

    Add declaration clas to my book store app project

---
+++ assets/js/book.js
@@ -0,0 +1,9 @@
+// Declare Main Class
+class Book{
+    static arraylist = [];
+    constructor(title, author,id){
+        this.title = title;
+        this.author = author;
+        this.id = id;
+    }
+}
