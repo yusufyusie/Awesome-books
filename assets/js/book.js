a5bb83a8c915b304722b3912ecb28f010ee73460
yusufyusie <yesuffenta023@gmail.com>
2023-07-03 21:30:23 +0300

    Add local storage method to save data to browser

--- assets/js/book.js
+++ assets/js/book.js
@@ -7,3 +7,28 @@ class Book{
         this.id = id;
     }
 }
+
+// Checking if localstorage is empty then add an empty array or store data into local storage
+// LOCAL STORAGE
+class LocalStore {
+    static storeBooks(){
+        let books = Book.arraylist = [...Book.arraylist, this];
+        const storeData = localStorage.getItem('local');
+        if(storeData === null) {
+            books = localStorage.setItem('Added Books', JSON.stringify([]));
+        } else {
+            books = JSON.parse(storeData);
+        }
+        return books;
+    }
+
+    static addBooks(newBook) {
+        const books = localStorage.storeBooks();
+        if(books){
+            books.push(newBook);
+            localStorage.setItem('local', JSON.stringify(books));
+        }
+    }
+}
+
+// 
