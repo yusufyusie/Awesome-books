345a06c8159c997a1948403819ca368fb47d62de
yusufyusie <yesuffenta023@gmail.com>
2023-07-04 11:06:08 +0300

    Refoctor the code

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,89 +1,65 @@
-// variables
 const form = document.querySelector('.add-book-form');
-const titleInput = document.querySelector('.title-book')
-const authorInput = document.querySelector('.author-book')
+const titleInput = document.querySelector('.title-book');
+const authorInput = document.querySelector('.author-book');
 const addButton = document.querySelector('.add-btn');
+const bookDetail = document.querySelector('.books-detail');
 const errorMsg = document.querySelector('.error-message');
-// Declare Main Class
+let newId = 0;
+
 class Book {
-     static arraylist = [];
-    constructor(title, author, id){
-        this.title = title;
-        this.author = author;
-        this.id = id;
-    }
-}
+  static arraylist = [];
 
-// Checking if localstorage is empty then add an empty array or store data into local storage
-// LOCAL STORAGE
-class LocalStore {
-    static storeBooks(){
-        let books;
-        const storeData = localStorage.getItem('local');
-        if(!storeData) {
-            books = localStorage.setItem('Added Books', JSON.stringify([]));
-        } else {
-            books = JSON.parse(storeData);
-        }
-        return books;
-    }
-
-    static addBooks(newBook) {
-        const books = localStorage.storeBooks();
-        if(books){
-            books.push(newBook);
-            localStorage.setItem('local', JSON.stringify(books));
-        }
-    }
-}
-
-class UI {
-    static showBooks() {
-      const books = LocalStore.getBooks();
-      books.forEach((newBook) => {
-        UI.addBookToList(newBook);
-      });
-    }
-  
-    static addBookToList(newBook) {
-      newBook.id = newId;
-      const bookInfo = `
-        <div id="${newId}">
-          <p class="book-position">"<span class="">${newBook.title}</span>" by <span class="">${newBook.author}</span></p>
-          <button id="${newId}" class="btn-remove">Remove</button>
-        </div>
-      `;
-      bookDetail.innerHTML += bookInfo;
-      newId += 1;
-    }
-  
-    static deleteBookFromList(e) {
-      e.parentElement.remove();
-    }
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
   }
 
-// Add books
-addButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    const books = LocalStore.storeBooks();
-    const newTitle = titleInput.value;
-    const newAuthor = authorInput.value;
-    let newId;
-    const len = books.length;
-    if(len === 0 || len === null) {
-        newId = 0;
-    } else {
-        newId = books[len-1].id + 1;
-    }
-    addNewBook = new Book(newTitle, newAuthor, newId);
-    if(newTitle.length !==0 && newAuthor.length !==0){
-        localStorage.addBooks(addNewBook);
-        UI.addBookToList(newBook);
-        errorMsg.style.display = 'none';
-    }  else {
-        errorMsg.style.display = 'block';
-      }
-      form.reset();
+  static showBooks() {
+    const books = Book.storeBooks();
+    books.forEach((addNewBook) => {
+      Book.addBookToList(addNewBook);
     });
+  }
 
-    document.addEventListener('DOMContentLoaded', UI.showBooks);
+  static addBookToList(addNewBook) {
+    addNewBook.id = newId;
+    const bookInfo = `
+      <div id="${newId}">
+        <p class="book-position">"<span class="">${addNewBook.title}</span>" by <span class="">${addNewBook.author}</span></p>
+        <button id="${newId}" class="btn-remove">Remove</button>
+      </div>
+    `;
+    bookDetail.innerHTML += bookInfo;
+    newId += 1;
+  }
+
+  static deleteBookFromList(e) {
+    e.parentElement.remove();
+  }
+}
+
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const books = Book.storeBooks();
+  const newTitle = titleInput.value;
+  const newAuthor = authorInput.value;
+  let newId;
+  const len = books.length;
+  if (len === 0 || len === null) {
+    newId = 0;
+  } else {
+    newId = books[len - 1].id + 1;
+  }
+  const addNewBook = new Book(newTitle, newAuthor, newId);
+  if (newTitle.length !== 0 && newAuthor.length !== 0) {
+    Book.addBooks(addNewBook);
+    Book.addBookToList(addNewBook);
+    errorMsg.style.display = 'none';
+  } else {
+    errorMsg.style.display = 'block';
+  }
+  form.reset();
+});
+
+document.addEventListener('DOMContentLoaded', Book.showBooks);
